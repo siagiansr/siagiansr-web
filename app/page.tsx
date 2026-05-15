57ab3ace99a077a538315f9a5f500bdd4f1c8a0e
sandtmiller <sandtmiller@msn.com>
2026-05-15 15:50:34 +0700

    fix: homepage redirect to 404

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { notFound } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">siagiansr.me</h1>
-    </main>
-  )
+  notFound()
 }
